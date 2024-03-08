6787b0d1ddd7aebb653e344aa4d535a602879a8b
Krzysiek <krzysieks.not.real.email@gmail.com>
2024-03-08 21:09:29 +0100

    Unused imports and code removed from BaseTest.java

--- src/test/java/org/cypress/example/BaseTest.java
+++ src/test/java/org/cypress/example/BaseTest.java
@@ -2,13 +2,11 @@ package org.cypress.example;
 
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
-import io.restassured.specification.ResponseSpecification;
 import org.cypress.example.models.User;
 
 import static org.hamcrest.Matchers.containsString;
@@ -119,17 +117,10 @@ public class BaseTest {
     }
 
     static class SpecBuilder {
-        static String token="";
         public static RequestSpecification getRequestSpec(){
             return new RequestSpecBuilder().setBaseUri(baseUri)
                     .addHeader("Cookie",cookieValue)
                     .setContentType(ContentType.JSON).log(LogDetail.ALL).build();
-        };
-
-        public static ResponseSpecification getResponseSpec(){
-            return new ResponseSpecBuilder().
-                    log(LogDetail.ALL).
-                    build();
         }
     }
 }
