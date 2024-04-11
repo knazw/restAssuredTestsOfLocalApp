5f3a41e1fb398e25ec454226f035e5c60869ff2c
Krzysiek <krzysieks.not.real.email@gmail.com>
2024-04-11 13:57:29 +0200

    Files renamed.

--- src/test/java/org/cypress/example/bdd/LoginGlueFile.java
+++ src/test/java/org/cypress/example/bdd/LoginGlueFile.java
@@ -7,11 +7,11 @@ import org.junit.platform.suite.api.Suite;
 
 @Suite
 @IncludeEngines("cucumber")
-@ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME,value = "src/test/resources/featureFiles/FirstBdd.feature")
+@ConfigurationParameter(key = Constants.FEATURES_PROPERTY_NAME,value = "src/test/resources/featureFiles/LoginBdd.feature")
 @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME,value = "org/cypress/example/bdd")
 @ConfigurationParameter(key = Constants.EXECUTION_DRY_RUN_PROPERTY_NAME,value = "false")
 @ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME,value = "pretty, html:target/cucumber-report/cucumber.html")
 @ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty,io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm")
 @ConfigurationParameter(key = Constants.PLUGIN_PUBLISH_QUIET_PROPERTY_NAME, value = "true")
-public class FirstGlueFile {
+public class LoginGlueFile {
 }
