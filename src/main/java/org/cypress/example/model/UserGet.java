897a7d472b1518b0a24f2014cddae1499725b8aa
Krzysiek <krzysieks.not.real.email@gmail.com>
2024-04-12 11:12:07 +0200

    New tests for registration process.

---
+++ src/main/java/org/cypress/example/model/UserGet.java
@@ -0,0 +1,157 @@
+package org.cypress.example.model;
+
+public class UserGet {
+    public String id;
+    public String uuid;
+    public String firstName;
+    public String lastName;
+    public String username;
+    public String password;
+    public String email;
+    public String phoneNumber;
+    public String avatar;
+    public String defaultPrivacyLevel;
+    public int balance;
+    public String createdAt;
+    public String modifiedAt;
+
+    public UserGet() {
+
+    }
+    public UserGet(String id, String uuid, String firstName, String lastName, String username, String password, String email, String phoneNumber, String avatar, String defaultPrivacyLevel, int balance, String createdAt, String modifiedAt) {
+        this.id = id;
+        this.uuid = uuid;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.avatar = avatar;
+        this.defaultPrivacyLevel = defaultPrivacyLevel;
+        this.balance = balance;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getDefaultPrivacyLevel() {
+        return defaultPrivacyLevel;
+    }
+
+    public void setDefaultPrivacyLevel(String defaultPrivacyLevel) {
+        this.defaultPrivacyLevel = defaultPrivacyLevel;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(String modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
+
+
+    /*
+                "id": "t45AiwidW",
+            "uuid": "6383f84e-b511-44c5-a835-3ece1d781fa8",
+            "firstName": "Edgar",
+            "lastName": "Johns",
+            "username": "Katharina_Bernier",
+            "password": "$2a$10$5PXHGtcsckWtAprT5/JmluhR13f16BL8SIGhvAKNP.Dhxkt69FfzW",
+            "email": "Norene39@yahoo.com",
+            "phoneNumber": "625-316-9882",
+            "avatar": "https://cypress-realworld-app-svgs.s3.amazonaws.com/t45AiwidW.svg",
+            "defaultPrivacyLevel": "public",
+            "balance": 168137,
+            "createdAt": "2019-08-27T23:47:05.637Z",
+            "modifiedAt": "2020-05-21T11:02:22.857Z"
+     */
+}
---
+++ src/test/resources/featureFiles/RegistrationBdd.feature
@@ -0,0 +1,44 @@
+Feature: Registration of an user
+
+  Background:
+    Given Following user "userForChecks"
+    And "userForChecks" is created
+    And 201 response code is received
+    And Json in response body matches createdUser.json
+    And Response object is properly validated as an user object of an user "userForChecks"
+    When "userForChecks" starts to login with credentials
+    Then 200 response code is received
+    And Correct user object is received
+    And Cookie can be obtained from response header
+
+
+  Scenario Outline: Registration of an user without all data is possible (bug? of this software)
+    Given Following user "<username>"
+    When "<username>" is created with not all data provided in request based on "<file>"
+    Then <status code> response code is received
+    And Response object is validated with a file "<schema>"
+
+    Given Following user "<username1>"
+    And "<username1>" is created
+    And 201 response code is received
+    And Json in response body matches createdUser.json
+    And Response object is properly validated as an user object of an user "<username1>"
+    When "<username1>" starts to login with credentials
+    Then 200 response code is received
+    And Correct user object is received
+    And Cookie can be obtained from response header
+
+    And Get request for users list is sent
+    And Response contains "<username>"
+    Examples:
+      | username | file                                    | status code | username1 | schema                                                            |
+      | username | RegistrationInvalid/EmptyFirstName.json | 201         | username1 | testDataResources/RegistrationInvalid/schemas/EmptyFirstName.json |
+      | username | RegistrationInvalid/EmptyLastname.json  | 201         | username1 | testDataResources/RegistrationInvalid/schemas/EmptyLastName.json  |
+#      | username | RegistrationInvalid/EmptyUsername.json  | 201         | Bad Request | username1 | testDataResources/RegistrationInvalid/schemas/EmptyUserName.json  |
+
+  Scenario: Registration of an user without password is not possible
+    Given Following user "username"
+    When "username" is created with not all data provided in request based on "RegistrationInvalid/EmptyPassword.json"
+    Then 500 response code is received
+    And Get request for users list is sent
+    And Response does not contain object for "<username>"
